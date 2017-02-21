bbba923318d76d8ecf1662061c2af00818e2a6ee
ORIDASHI-SERVER\brett <brett.esler@oridashi.com.au>
2017-02-21 11:16:27 +1100

    Added indigenous status extension + valueset

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 echo You have to edit the path to the publisher...
 pause
-java -jar C:\work\org.hl7.fhir\build\publish\org.hl7.fhir.igpublisher.jar -ig base.json
+"C:\Program Files (x86)\Java\jre1.8.0_121\bin\java.exe" -Xmx1024m -jar "D:\Oridashi\Repositories\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json
 
 pause
