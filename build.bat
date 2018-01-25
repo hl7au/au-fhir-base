f55e870fe6275b0ecfa0cc6dfdb54474be92cba2
ORIDASHI-SERVER\brett <brett.esler@oridashi.com.au>
2018-01-25 11:07:11 +1100

    specialties

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 echo You have to edit the path to the publisher...
 pause
-"C:\Program Files (x86)\Java\jre1.8.0_121\bin\java.exe" -Xmx1024m -jar "D:\Oridashi\Repositories\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json
+"C:\Program Files (x86)\Java\jre1.8.0_121\bin\java.exe" -Xmx1024m -jar "D:\Oridashi\Repositories\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json -watch
 
 pause
