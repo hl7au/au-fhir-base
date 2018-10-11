304fc1ab8eebaa8a468de1e921a19148a14c125b
ORIDASHI-SERVER\brett <brett.esler@oridashi.com.au>
2018-10-12 00:10:31 +1100

    reinstate

---
+++ tx/update.cmd
@@ -0,0 +1,24 @@
+curl -d "@CodeSystem-au-hl7v2-0203.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/au-hl7v2-0203
+curl -d "@CodeSystem-gtin.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/gtin
+curl -d "@CodeSystem-icpc2plus.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/icpc2plus
+curl -d "@CodeSystem-medication-type.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/medication-type
+curl -d "@CodeSystem-mims.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/mims
+curl -d "@CodeSystem-pbs-item.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/CodeSystem/pbs-item
+ 
+
+curl -d "@ValueSet-amt-ctpp-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-ctpp-codes
+curl -d "@ValueSet-amt-mp-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-mp-codes
+curl -d "@ValueSet-amt-mpp-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-mpp-codes
+curl -d "@ValueSet-amt-mpuu-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-mpuu-codes
+curl -d "@ValueSet-amt-tp-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-tp-codes
+curl -d "@ValueSet-amt-tpp-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-tpp-codes
+curl -d "@ValueSet-amt-tpuu-codes.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/amt-tpuu-codes
+curl -d "@ValueSet-au-hl7v2-0203.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/au-hl7v2-0203
+curl -d "@ValueSet-grounds-for-concurrent-supply.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/grounds-for-concurrent-supply
+curl -d "@ValueSet-gtin.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/gtin
+curl -d "@ValueSet-icpc2plus.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/icpc2plus
+curl -d "@ValueSet-medication-type.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/medication-type
+curl -d "@ValueSet-mims.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/mims
+curl -d "@ValueSet-pbs-item.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/pbs-item
+curl -d "@ValueSet-snomed-healthcareservice-services.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/snomed-healthcareservice-services
+curl -d "@ValueSet-snomed-practitioner-role.xml" -H "Content-Type: application/fhir+xml" -X PUT http://tx.hl7.org.au/fhir/ValueSet/snomed-practitioner-role
