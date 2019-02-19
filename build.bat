4cf4ef36acfb0c25c842a37bf7279483bf7f5563
ORIDASHI-SERVER\brett <brett.esler@oridashi.com.au>
2019-02-20 10:44:27 +1100

    snapshot publication

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 echo You have to edit the path to the publisher...
 pause
-"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -Xmx1620m -jar "D:\Oridashi\Repositories\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json -watch
+"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -Xmx1620m -jar "D:\Oridashi\Repositories\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json -publish http://hl7.org.au/fhir/base2019Feb -watch
 
 pause
