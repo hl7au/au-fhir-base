0c8113672055498e954aea3c04cdef81ff5ce4cb
Brett Esler <brett.esler@oridashi.com.au>
2019-08-07 22:47:27 +1000

    build base2019Aug

--- buildv.bat
+++ buildv.bat
@@ -2,6 +2,7 @@
 
 echo You have to edit the path to the publisher...
 pause
-"C:\Program Files\Java\jre1.8.0_211\bin\java.exe" -Xmx2048m -jar "org.hl7.fhir.igpublisher.jar" -ig ig.json -publish http://hl7.org.au/fhir/base/aubase1
+"C:\Program Files\Java\jre1.8.0_211\bin\java.exe" -Xmx2048m -jar "org.hl7.fhir.igpublisher.jar" -ig ig.json -publish http://hl7.org.au/fhir/base/base2019Aug
+
 
 pause
