237ce0b21bf1f0bff768d97c406cb3eae236cffc
Brett Esler <brett.esler@teamunicorn.net>
2023-11-15 16:02:53 +1100

    build script

--- buildv.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-
-echo You have to edit the path to the publisher...
-pause
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xmx4096m -jar "publisher.jar" -ig ig.ini -publish http://hl7.org.au/fhir/4.1.1-preview
-
-pause
