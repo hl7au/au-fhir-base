1a07cbb1e99d12bda750b3e52f1d4441b3eac504
dbojicic <dusica.bojicic@csiro.au>
2023-11-15 11:47:39 +1000

    4.1.2-preview markers

--- buildv.bat
+++ buildv.bat
@@ -2,6 +2,6 @@
 
 echo You have to edit the path to the publisher...
 pause
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xmx4096m -jar "publisher.jar" -ig ig.ini -publish http://hl7.org.au/fhir/4.1.1-preview
+"C:\Program Files\Java\jdk-17\bin\java.exe" -Xmx4096m -jar "publisher.jar" -ig ig.ini -publish http://hl7.org.au/fhir/4.1.2-preview
 
 pause
