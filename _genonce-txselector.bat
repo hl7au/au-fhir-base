84d70c0e4b4cb2c51f817a5c0eb8f5dc9f92fa50
Brett Esler <brett.esler@teamunicorn.net>
2024-05-08 16:10:17 +1000

    Create _genonce-txselector.bat

---
+++ _genonce-txselector.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+SET publisher_jar=publisher.jar
+SET input_cache_path=%CD%\input-cache
+
+
+:isonline
+ECHO We're online -tx https://tx.dev.hl7.org.au/fhir
+SET txoption= -tx https://txreg.azurewebsites.net/txdev
+
+:igpublish
+
+SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+
+IF EXIST "%input_cache_path%\%publisher_jar%" (
+	JAVA -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption% %*
+) ELSE If exist "..\%publisher_jar%" (
+	JAVA -jar "..\%publisher_jar%" -ig . %txoption% %*
+) ELSE (
+	ECHO IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
+)
+
+PAUSE
